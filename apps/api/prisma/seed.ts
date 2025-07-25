3ecf08e3f6fa519f0c611c295b8046071c809d13
ZawHlaingPhyoTsuki <tro2233zhp@gmail.com>
2025-07-25 11:49:19 +0700

    setup turbo and seed prisma

---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,68 @@
+import { faker } from '@faker-js/faker';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+function generateSlug(title: string): string {
+  return title
+    .toLocaleLowerCase()
+    .trim()
+    .replace(/ /g, '-') //Replace spaces with hyphens
+    .replace(/[^\w-]+/g, ''); //Remove All non-word characters except hyphens
+}
+
+async function main() {
+  // Create users
+  const user = Array.from({ length: 10 }).map(() => ({
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    bio: faker.lorem.sentence(),
+    avatar: faker.image.avatar(),
+  }));
+
+  await prisma.user.createMany({
+    data: user,
+  });
+
+  // Create posts with comments
+  const posts = Array.from({ length: 40 }).map(() => ({
+    title: faker.lorem.sentence(),
+    slug: generateSlug(faker.lorem.sentence()),
+    content: faker.lorem.paragraphs(3),
+    thumbnail: faker.image.urlLoremFlickr(),
+    authorId: faker.number.int({ min: 1, max: 10 }),
+    published: true,
+  }));
+
+  await Promise.all(
+    posts.map(
+      async (post) =>
+        await prisma.post.create({
+          data: {
+            ...post,
+            comments: {
+              createMany: {
+                data: Array.from({ length: 20 }).map(() => ({
+                  content: faker.lorem.sentence(),
+                  authorId: faker.number.int({ min: 1, max: 10 }),
+                })),
+              },
+            },
+          },
+        }),
+    ),
+  );
+
+  console.log('Seeding completed successfully');
+}
+
+main()
+  .then(() => {
+    prisma.$disconnect();
+    process.exit(0);
+  })
+  .catch((e) => {
+    prisma.$disconnect();
+    console.error(e);
+    process.exit(1);
+  });
